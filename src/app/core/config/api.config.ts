82375353a0bb3e1fce69d1a93e40f2b9738db009
Sebastian Perez <edgar.perez02@uptc.edu.co>
2025-11-23 23:14:12 -0500

    chore: subir todos los cambios recientes

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,2 +1,2 @@
 // src/app/core/config/api.config.ts
-export const BASE_API_URL = 'https://785cszwrs2.execute-api.us-east-2.amazonaws.com/api';
+export const BASE_API_URL = 'https://785cszwzs2.execute-api.us-east-2.amazonaws.com/api';
